095c91304f973d11b620248417fc0b29f2346d24
Islam <isxmadgmail.com>
2022-02-08 01:36:44 +0300

    test 6

--- #src/js/script.js
+++ #src/js/script.js
@@ -190,7 +190,7 @@ document.addEventListener('DOMContentLoaded', () => {
     filterMobile();
     cardGallery();
 
-    console.log('test new lib scroll 5');
+    console.log('test new lib scroll 6');
 
 
     
